e017612eae5f3093ce71d9ca57320db4830d5ad1
lucas <lucas.f.roldan@gmail.com>
2025-11-20 10:28:16 -0300

    Repaso finalizado con comentarios

--- obj/Debug/net8.0/tl2-tp8-2025-LucasFR-TH.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp8-2025-LucasFR-TH.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp8-2025-LucasFR-TH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744d7f1c53dd59760d08c89da14f60fb3cd749ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67de4116a2881ff4f523ab5bb3c5dde048e52b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp8-2025-LucasFR-TH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp8-2025-LucasFR-TH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
